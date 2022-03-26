bfdc411ee47289c8dd40f08cfbafa8b719a7065f
Ravi Kumar Ranga <ravimcaanurag@gmail.com>
2022-03-26 20:21:34 +0530

    KV Changes done

--- MVCLab001/Controllers/CourseController.cs
+++ MVCLab001/Controllers/CourseController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Azure.Core;
+using Azure.Identity;
+using Azure.Security.KeyVault.Secrets;
+using Microsoft.AspNetCore.Mvc;
 using MVCLab001.Services;
 using System;
 using System.Collections.Generic;
@@ -18,6 +21,22 @@ namespace MVCLab001.Controllers
         [Route("Course/Display")]
         public IActionResult Index()
         {
+            SecretClientOptions options = new SecretClientOptions()
+            {
+                Retry =
+        {
+            Delay= TimeSpan.FromSeconds(2),
+            MaxDelay = TimeSpan.FromSeconds(16),
+            MaxRetries = 5,
+            Mode = RetryMode.Exponential
+         }
+            };
+            var client = new SecretClient(new Uri("https://labkv001.vault.azure.net/"), new DefaultAzureCredential(), options);
+
+            KeyVaultSecret secret = client.GetSecret("sqlcnn");
+
+            string secretValue = secret.Value;
+            ViewBag.secret = secretValue;
 
             return View(ser.getCourses());
         }
--- MVCLab001/Services/courseService.cs
+++ MVCLab001/Services/courseService.cs
@@ -5,13 +5,16 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Data.SqlClient;
 using Microsoft.Extensions.Configuration;
+using Azure.Identity;
+using Azure.Security.KeyVault.Secrets;
+using Azure.Core;
 
 namespace MVCLab001.Services
 {
     public class courseService
     {
 
-        private readonly  IConfiguration config;
+        private readonly IConfiguration config;
         public courseService(IConfiguration _config)
         {
             config = _config;
@@ -19,6 +22,9 @@ namespace MVCLab001.Services
         public List<CourseMdl> getCourses()
         {
             List<CourseMdl> courses = new List<CourseMdl>();
+
+           
+            
 
             using (SqlConnection cn = new SqlConnection(config.GetConnectionString("sqlcnn")))
             {
@@ -28,14 +34,14 @@ namespace MVCLab001.Services
                 SqlDataReader reader = sqlCommand.ExecuteReader();
                 while (reader.Read())
                 {
-                    courses.Add(new CourseMdl() { courseID =reader.GetInt32(0), cname=reader.GetString(1), Rating=reader.GetInt32(2) });
+                    courses.Add(new CourseMdl() { courseID = reader.GetInt32(0), cname = reader.GetString(1), Rating = reader.GetInt32(2) });
                 }
 
                 reader.Close();
                 cn.Close();
-               
+
             }
-            
+
             return courses;
         }
     }
