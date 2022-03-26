4e7fef81c783b4fc4f55fb8edd31e51310ef53a9
Ravi Kumar Ranga <ravimcaanurag@gmail.com>
2022-03-26 22:20:13 +0530

    changes done

--- MVCLab001/Controllers/CourseController.cs
+++ MVCLab001/Controllers/CourseController.cs
@@ -21,22 +21,11 @@ namespace MVCLab001.Controllers
         [Route("Course/Display")]
         public IActionResult Index()
         {
-            SecretClientOptions options = new SecretClientOptions()
-            {
-                Retry =
-        {
-            Delay= TimeSpan.FromSeconds(2),
-            MaxDelay = TimeSpan.FromSeconds(16),
-            MaxRetries = 5,
-            Mode = RetryMode.Exponential
-         }
-            };
-            var client = new SecretClient(new Uri("https://labkv001.vault.azure.net/"), new DefaultAzureCredential(), options);
-
-            KeyVaultSecret secret = client.GetSecret("sqlcnn");
-
-            string secretValue = secret.Value;
-            ViewBag.secret = secretValue;
+            var credential = new DefaultAzureCredential();
+            var client = new SecretClient(new Uri("https://labkv001.vault.azure.net/"), credential);
+            var secret = client.GetSecret("sqlcnn");
+            string actualSecret = secret.Value.Value;
+            ViewBag.secret = actualSecret;
 
             return View(ser.getCourses());
         }
